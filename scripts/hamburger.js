9c2108c2a8cbfdb0514b3cfa1fa238ce5f86626d
vsyang <yang.vanessa85@gmail.com>
2024-09-16 19:26:17 -0600

    css starting, fixed hamburger js

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,7 +1,7 @@
-const hamburgerButton = document.querySelector('#hamburgerButton');
+const hamburgerButton = document.querySelector('#hamburgerMenu');
 const navLink = document.querySelector('.navLinks');
 
 hamburgerButton.addEventListener('click', () => {
     navLink.classList.toggle('open');
     hamburgerButton.classList.toggle('open');
-})
+});
