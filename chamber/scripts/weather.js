14f16366c45a1aeda8430a79f4a6ed13788e779c
vsyang <yang.vanessa85@gmail.com>
2024-09-26 21:46:41 -0600

    fixing weather js

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,4 +1,7 @@
 const weather = document.querySelector('.weather-info');
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const description = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/';
 const apiKey = 'c8e24775a530c3c13e6c5c81a4f0e243';
 
@@ -24,69 +27,23 @@ async function apiFetch() {
 }
 
 function displayResults(data, forecast) {
-    let currentTable = document.createElement('table');
-    let currentHeader = document.createElement('thead');
-    let currentBody = document.createElement('tbody');
-    let currentRow = document.createElement('tr');
-    let currentHeadSpan = document.createElement('th');
-    
-    let currentData = document.createElement('td');
-    let icon = document.createElement('img');
-    let iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let description = data.weather[0].description;
-    
-    currentHeadSpan.textContent = 'Current Weather';
-    currentHeadSpan.setAttribute('colspan', '3');
-    currentHeader.appendChild(currentHeadSpan);
-    currentTable.appendChild(currentHeader);
-
-    icon.setAttribute('src', iconSrc);
-    icon.setAttribute('alt', description);
-    icon.setAttribute('width', 50);
-    icon.setAttribute('height', 50);
-    icon.classList.add('weather-icon');
-    currentData.appendChild(icon);
-    currentRow.appendChild(currentData);
-
-    currentData = document.createElement('td');
-    currentData.innerHTML = `${data.main.temp}°F`;
-    currentRow.appendChild(currentData);
-
-    currentData = document.createElement('td');
-    currentData.innerHTML = `${data.weather[0].description}`;
-    currentRow.appendChild(currentData);
-
-    currentBody.appendChild(currentRow);
-    currentTable.appendChild(currentBody);
+    const heading = document.createElement('h3');
+    heading.textContent = 'Current Weather';
+    let current = document.querySelector('.current-weather');
+    current.textContent = `${data.main.temp}°F`
+    const icon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let capDescription = data.weather[0].description;
+    weatherIcon.setAttribute('src', icon);
+    weatherIcon.setAttribute('alt', capDescription);
+    description.textContent = `${capDescription}`;
+    current.appendChild(heading);
+    current.appendChild(currentTemp);
+    current.appendChild(weatherIcon);
+    current.appendChild(description);
 
 
-    let forecastTable = document.createElement('table');
-    let forecastHead = document.createElement('thead');
-    let forecastBody = document.createElement('tbody');
-    let forecastRow = document.createElement('tr');
-    let forecastContext = document.createElement('th');
-    forecastContext.textContent = '3-Day Forecast';
-    forecastContext.setAttribute('colspan', '3');
-    forecastHead.appendChild(forecastContext);
-    forecastTable.appendChild(forecastHead);
+    let weatherForecast = document.querySelector('.weather-forecast');
 
-    for (let i = 0; i < 3; i++) {
-        let forecastData = document.createElement('td');
-        let icon = document.createElement('img');
-        let iconSrc = `https://openweathermap.org/img/w/${forecast.list[i].weather[0].icon}.png`;
-        let description = forecast.list[i].weather[0].description;
-        icon.setAttribute('src', iconSrc);
-        icon.setAttribute('alt', description);
-        forecastData.appendChild(icon);
-        forecastData.innerHTML += `<br>${forecast.list[i].main.temp}°F`;
-        forecastRow.appendChild(forecastData);
-    }
-    forecastBody.appendChild(forecastRow);
-    forecastTable.appendChild(forecastBody);
-    forecastTable.setAttribute('id', 'forecast');
-    currentTable.setAttribute('id', 'current');
-    weather.appendChild(currentTable);
-    weather.appendChild(forecastTable);
 }
 
 apiFetch();
