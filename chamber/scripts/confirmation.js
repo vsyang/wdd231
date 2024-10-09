f23ad1dd7a9dfd25bdc9133b80ee9191b8e33d95
vsyang <yang.vanessa85@gmail.com>
2024-10-08 23:52:22 -0600

    css done

--- chamber/scripts/confirmation.js
+++ chamber/scripts/confirmation.js
@@ -17,9 +17,9 @@ function show(info) {
 const showInfo = document.querySelector('#results');
 
 showInfo.innerHTML = `
-<h3>Thank you ${show('fname')} ${show('lname')}!</h3>
+<h3>We'll talk to you soon ${show('fname')} ${show('lname')}!</h3>
 <br><p>We have received the following information:</p>
-<p>Your Email: <a href=mailto:${show('email')} id="email">${show('email')}</a></p>
+<br><p>Your Email: <a href=mailto:${show('email')} id="email">${show('email')}</a></p>
 <p>Your Phone: ${show('phone')}</p>
 <p>Your Business: ${show('business')}</p>
 <p>Date and Time Completed: ${show('timestamp')}</p>
