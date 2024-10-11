0018675aaa0fdd4fccd2831ee79e3732a87f3dcc
vsyang <yang.vanessa85@gmail.com>
2024-10-10 20:30:03 -0600

    work on css tomorrow

---
+++ chamber/scripts/welcome.js
@@ -0,0 +1,28 @@
+function calculateDays(currentTime, lastVisit) {
+    const msPerDay = 24 * 60 * 60 * 1000;
+    const timeDifference = currentTime - lastVisit;
+    return Math.floor(timeDifference / msPerDay);
+}
+
+function displayMessage() {
+    const message = document.getElementById('welcome');
+    const currentTime = Date.now();
+    const lastVisit = localStorage.getItem('lastVisit');
+
+    if (!lastVisit) {
+        message.textContent = "Welcome! Let us know if you have any questions. "
+    } else {
+        const daysDifference = calculateDays(currentTime, lastVisit);
+
+        if (daysDifference < 1) {
+            message.textContent = "Back so soon! Great to see you!";
+        } else {
+            const plural = daysDifference === 1 ? '' : 's';
+            message.textContent = `You last visited ${daysDifference} day${plural} ago.`;
+        }
+    }
+
+    localStorage.setItem('lastVisit', currentTime);
+}
+
+window.onload = displayMessage;
