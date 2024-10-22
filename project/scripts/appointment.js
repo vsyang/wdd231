e058660e44a682946b858c35ab05800ef887d378
vsyang <yang.vanessa85@gmail.com>
2024-10-21 20:47:30 -0600

    phone.js almost figured out

--- project/scripts/appointment.js
+++ project/scripts/appointment.js
@@ -1,6 +1,17 @@
-//take care of the form - remember info input from user, welcome message, check input before allowing submit button
-
 import {displayMessage} from "./welcome.js";
+import { accept } from "./phone.js";
 
+//welcome message
 const welcome = document.querySelector('#welcome');
 welcome.innerHTML = displayMessage();
+
+//phone validator
+const phoneNumber = document.querySelector('#phone');
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    const value = phoneNumber.value;
+    if(!accept(value)) {
+        event.preventDefault();
+    }
+});
+
--- project/scripts/phone.js
+++ project/scripts/phone.js
@@ -1,1 +1,17 @@
-//get phone number
+// const phoneNumber = document.querySelector('#phone');
+// const form = document.querySelector('form');
+
+export function accept(phoneNumber) {
+    const pattern = /(?:(\+1)[ -])?\(?\d{3}\)?[ -]?(\d{3})[ -]?(\d{4})/;
+
+    if (phoneNumber.length < 10) {
+        alert('Please enter 10-digit phone number');
+        return false;
+    }
+
+    if (!pattern.test(phoneNumber)) {
+        alert('Please enter a valid phone number');
+        return false;
+    }
+    return true;
+}
